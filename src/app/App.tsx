08c09f531b8b8f82e1320c3e9067bce675e63d82
Anastasia Polozova <polozova.anastasi@gmail.com>
2026-02-11 15:18:41 -0300

    feat: setup project infrastructure with Webpack, TypeScript and FSD structure

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App: React.FC = () => {
+  return (
+    <div>
+      <h1>User Database App</h1>
+      <p>Application is starting...</p>
+    </div>
+  );
+};
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@/app': path.resolve(__dirname, 'src/app'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/features': path.resolve(__dirname, 'src/features'),
+      '@/entities': path.resolve(__dirname, 'src/entities'),
+      '@/shared': path.resolve(__dirname, 'src/shared'),
+      '@/widgets': path.resolve(__dirname, 'src/widgets'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
